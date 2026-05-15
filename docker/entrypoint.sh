84d7062e11580719b89b0f5dab1b74175610066a
MolotoveDev <alex.carrasco005@gmail.com>
2026-05-15 18:42:29 +0200

    fix deploy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,5 +6,5 @@ php bin/console cache:clear --no-warmup --env=prod
 php bin/console cache:warmup --env=prod
 chown -R www-data:www-data var/
 echo "[entrypoint] Iniciando Apache..."
-
+echo "[entrypoint] PHP ve APP_ENV: $(php -r "echo getenv('APP_ENV');")"
 exec "$@"
