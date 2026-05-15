158d710ff7610b98b93c5ee5d93cd85e74e34b2a
MolotoveDev <alex.carrasco005@gmail.com>
2026-05-15 18:30:59 +0200

    permisos carpeta

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 echo "[entrypoint] APP_ENV=${APP_ENV:-prod}"
 php bin/console cache:clear --no-warmup --env=prod
 php bin/console cache:warmup --env=prod
+chown -R www-data:www-data var/
 echo "[entrypoint] Iniciando Apache..."
 
 exec "$@"
